8ef19064616aea1d5a502c6e701fd4cc0f25c23b
Raphael Ferreira <raphanallah@hotmail.com>
2022-07-28 14:12:18 -0300

    atualização verificações MercadoPago.php

--- MercadoPago.php
+++ MercadoPago.php
@@ -2,25 +2,22 @@
 include_once('_private/config.inc.php');
 
 
-  $json = file_get_contents('php://input');
-  $data = json_decode($json);
+$json = file_get_contents('php://input');
+$data = json_decode($json);
 
 //echo $json;
 
-  //echo $data->payment->id; 
-  //echo $data->payment->state; 
-  
-  $chave = $data->additional_info->external_reference;
+//echo $data->payment->id; 
+//echo $data->payment->state; 
 
-if ($data->payment->state == "approved")
-{
+$chave = $data->external_reference;
+
+if ($data->status == "approved") {
   $ObjBd = new User();
   //retorna uma questão não respondida de uma lista de questões, ou nenhuma se todas já estão respondidas
   $sql = "UPDATE User set License = 'JORNADA_1' where id = $chave";
   echo $sql;
-  $ok = ($ObjBd->query($sql) > 0)? 'Dados atualizados com sucesso!' : 'erro ao atualizar.';
+  $ok = ($ObjBd->query($sql) > 0) ? 'Dados atualizados com sucesso!' : 'erro ao atualizar.';
 
   echo $ok;
 }
-
-?>
