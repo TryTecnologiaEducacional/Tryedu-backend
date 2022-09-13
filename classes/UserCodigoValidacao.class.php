e355a87694a56e936df93c22a5942b6a2ce7f71b
Fernando Dangelo <fernando@aspbrasil.net>
2022-09-13 12:15:38 -0300

    ajustes para disparo de email STMP phpmailer

--- classes/UserCodigoValidacao.class.php
+++ classes/UserCodigoValidacao.class.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
 class UserCodigoValidacao extends Tabela {
   protected $tabela = 'UserCodigoValidacao';
   protected $chavePrimaria = 'id';
@@ -18,9 +24,16 @@ $dataStamp = $data->format("Y-m-d H:i:s");
 
 $sql = "INSERT INTO `teentok_teste`.`UserCodigoValidacao` ( `email`, `codigo`, `dataValidade`, `ativado`, `metodoValidacao`) VALUES ('$email', '$codigo', '$dataStamp', false, 'email');";
 $this->query($sql);
-$arr = array('true','Código gerado e enviado com sucesso.');
+$arr = array('true','Código gerado e enviado com sucesso.'.$codigo);
 
+$sender = 'fernando@aspbrasil.net';
+$senderName = 'Fernando Dangelo';
 
+$usernameSmtp = 'fernando@aspbrasil.net';
+$passwordSmtp = 'oDnanref#22';
+$configurationSet = 'ConfigSet';
+$host = 'smtp.gmail.com';
+$port = 587;
 
 $cabecalho = '<html>
 <head>
@@ -30,26 +43,44 @@ $cabecalho = '<html>
 <body>';
 $rodape = '</body></html>';
 
-$ql = (PHP_OS == 'WINNT')? "\r\n" : "\n";//definindo quebra de linha conforme SO do servidor
-$headers = 'From: TRYGAME <tryeducacao@gmail.com>'. $ql;//cc, cco....
-$headers .= 'MIME-Version: 1.0' . $ql;
-$headers .= 'Content-type: text/html; charset=UTF-8' . $ql;
+require './mailer/PHPMailerAutoload.php';
+
 $to = $email;
 $subject = 'Cadastro TryEdu';
 $conteudo = '<p>Código de Ativação:'.$codigo.'</p> Nós protegemos você e seus dados, caso tenha alguma dúvida, pode acessar nossa política de privacidade: <a href="https://tryedu.com.br/politica-de-privacidade">https://tryedu.com.br/politica-de-privacidade</a></p>';
 $conteudo .= '<p>Para mais informações sobre o jogo basta acessar nosso site: <a href="http://umaaventuranaescola.com.br/">http://umaaventuranaescola.com.br/</a></p>';
 $conteudo .= '<p>Nos não enviamos anúncios, você não receberá e-mail de publicidade.</p>';
 $message = $cabecalho . $conteudo . $rodape;
-$envioEmail = 0;
-try{
-  $envioEmail = mail($to, $subject, $message, $headers);
-}
-catch(Exception $e)
-{}
 
-if ($envioEmail != true)
-{
-  $arr = array('false','Erro no envio do email.');
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+$mail->Debugoutput = 'html';
+$mail->IsHTML(true);
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "fernando@aspbrasil.net";
+$mail->Password = "oDnanref#22";
+$mail->setFrom('from@example.com', 'First Last');
+$mail->addAddress('fernando@aspbrasil.net', 'Woods');
+$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->Body = $message;
+
+if (!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    $arr = array('false','Erro no envio do email.');
+} else {
+    echo 'Message has been sent';
 }
 
 return $arr;
