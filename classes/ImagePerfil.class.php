ffb953a8806769b779fd6459952cd3c66402b720
Raphael Ferreira <raphanallah@hotmail.com>
2022-10-25 09:51:24 -0300

    Create ImagePerfil.class.php

---
+++ classes/ImagePerfil.class.php
@@ -0,0 +1,8 @@
+<?php
+class ImagePerfil extends Tabela {
+  protected $tabela = 'ImagePerfil';
+  protected $chavePrimaria = 'id';
+  protected $legendas = array(
+                             );
+
+}
